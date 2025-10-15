89647f237f4d7632d71c16c5d35a7c17f4b5a24f
Richard Ulrich <richi@ulrichard.ch>
2025-10-15 19:58:18 +0200

    added a unit test

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 > #### ⚠️ Alpha Warning! ⚠️
 > This Rust crate is still in alpha stage. It is something I quickly put together if anyone needed it. I'm aiming to work on it as I need more features.
 
-Currently it can only convert trips downloaded from the Candela WebApp (https://app.candela.com/trips) from xlsx format to gpx.
+Currently it can only convert trips downloaded from the [Candela](https://github.com/CandelaSpeedBoat) WebApp (https://app.candela.com/trips) from xlsx format to gpx.
 In the future I plan to add features to query the Candela API used in the WebApp.
 Candelatte aims to be both a CLI and a Rust crate for interacting with the Candela API.
 This part was inspired by Teslatte (https://github.com/gak/teslatte)
@@ -22,3 +22,6 @@ file written to "/home/richi/Downloads/boat_data_2025-08-25_09-33-39_2025-08-24 
 
 Licensed under the GPL
 
+## Minimum Supported Rust Version (MSRV)
+
+This library should always compile with Rust **1.85**.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use calamine::{RangeDeserializerBuilder, Reader, Xlsx, open_workbook};
 use geo_types::{Point, coord};
-use gpx::{Gpx, GpxVersion, Track, TrackSegment, Waypoint};
+use gpx::{Gpx, GpxVersion, Link, Track, TrackSegment, Waypoint};
 use std::{error::Error, fs::File, io::BufWriter, path::Path};
 use time::{OffsetDateTime, format_description::well_known::Rfc3339};
 
@@ -18,30 +18,72 @@ fn main() {
     let mut out_path = in_path.to_path_buf();
     out_path.set_extension("gpx");
 
-    xlsx_to_gpx(&in_path, &out_path).unwrap();
+    xlsx_to_gpx(in_path, &out_path).unwrap();
 }
 
 fn xlsx_to_gpx(in_path: &Path, out_path: &Path) -> Result<(), Box<dyn Error>> {
-    // prep xlsx
+    // read xlsx
     let mut workbook: Xlsx<_> = open_workbook(in_path)?;
     let sheets = workbook.worksheets();
-    let range = workbook.worksheet_range(&sheets[0].0).unwrap();
+    let range = workbook.worksheet_range(&sheets[0].0)?;
 
-    let mut iter = RangeDeserializerBuilder::new().from_range(&range).unwrap();
+    // convert track points
+    let trackpoints = RangeDeserializerBuilder::new()
+        .from_range(&range)?
+        .map(|record| {
+            let (_, dts, _, _, lat, lon, _, _, _, knt): (
+                String,
+                String,
+                String,
+                String,
+                f64,
+                f64,
+                String,
+                String,
+                f64,
+                f64,
+            ) = record?;
 
-    // prep gpx
-    let track_segment = TrackSegment { points: vec![] };
+            let geo_point: Point = coord! { x: lon, y: lat }.into();
+            let mut wp = Waypoint::new(geo_point);
+
+            let mut dts = dts.replace(" ", "T");
+            dts.truncate(23);
+            dts.push('Z');
+            let odt = OffsetDateTime::parse(&dts, &Rfc3339)?;
+            let ti = gpx::Time::from(odt);
+
+            wp.time = Some(ti);
+            wp.speed = Some(knt * 1.852); // to km/h
+
+            Ok(wp)
+        })
+        .collect::<Result<Vec<Waypoint>, Box<dyn Error>>>()?;
+
+    // write gpx
+    let links = [
+        "https://candela.com",
+        "https://app.candela.com",
+        "https://github.com/ulrichard/candelatte",
+        "https://github.com/CandelaSpeedBoat",
+    ]
+    .iter()
+    .map(|url| Link {
+        href: (*url).into(),
+        ..Default::default()
+    })
+    .collect::<Vec<_>>();
     let track = Track {
         name: Some(sheets[0].0.clone()),
-        comment: None,
-        description: Some("Track from a Candela C8 hydrofoil boat".into()),
+        description: Some("Track from a Candela C8 electric hydrofoil boat".into()),
         source: Some("https://app.candela.com/trips".into()),
-        links: vec![],
-        type_: None,
-        number: None,
-        segments: vec![track_segment],
+        links,
+        segments: vec![TrackSegment {
+            points: trackpoints,
+        }],
+        ..Default::default()
     };
-    let mut gpx = Gpx {
+    let gpx = Gpx {
         version: GpxVersion::Gpx11,
         creator: Some("candelatte".into()),
         metadata: None,
@@ -50,42 +92,38 @@ fn xlsx_to_gpx(in_path: &Path, out_path: &Path) -> Result<(), Box<dyn Error>> {
         routes: vec![],
     };
 
-    // Create file at path
-    let gpx_file = File::create(out_path).unwrap();
+    // write to file
+    let gpx_file = File::create(out_path)?;
     let buf = BufWriter::new(gpx_file);
-
-    // convert
-    while let Some(result) = iter.next() {
-        let (_, dts, _, _, lat, lon, _, _, _, knt): (
-            String,
-            String,
-            String,
-            String,
-            f64,
-            f64,
-            String,
-            String,
-            f64,
-            f64,
-        ) = result.unwrap();
-
-        let geo_coord = coord! { x: lon, y: lat };
-        let geo_point: Point = geo_coord.into();
-        let mut wp = Waypoint::new(geo_point);
-
-        let mut dts = dts.replace(" ", "T");
-        dts.truncate(23);
-        dts.push_str("Z");
-        let odt = OffsetDateTime::parse(&dts, &Rfc3339)?;
-        let ti = gpx::Time::from(odt);
-
-        wp.time = Some(ti);
-        wp.speed = Some(knt * 1.852); // to km/h
-        gpx.tracks[0].segments[0].points.push(wp);
-    }
-
-    gpx::write(&gpx, buf).unwrap();
+    gpx::write(&gpx, buf)?;
     println!("file written to {:?}", out_path);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    fn get_test_track_file(track_name: &str) -> PathBuf {
+        let test_dir = Path::new(file!()).canonicalize().unwrap();
+        let test_dir = test_dir.as_path().parent().unwrap();
+        let test_dir = test_dir.parent().unwrap();
+        let track_file = test_dir.join("tests/data/".to_string() + track_name);
+        assert!(
+            track_file.exists(),
+            "File not found: {}",
+            track_file.to_str().unwrap()
+        );
+        track_file.to_path_buf()
+    }
+
+    #[test]
+    fn convert_xlsx_to_gpx() {
+        let track_file = get_test_track_file("boat_data_2025-06-29_19-41-12_trips_C8-S61.xlsx");
+        let temp_dir = temp_dir::TempDir::new().unwrap();
+
+        xlsx_to_gpx(&track_file, &temp_dir.child("boat_trip.gpx")).unwrap();
+    }
 }
